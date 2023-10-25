f16a88917868f9631df6c3d369b241088ab41e66
imariel2d <imariel2d@gmail.com>
2023-10-24 18:25:25 -0700

    Initial commit

---
+++ @types/developer.ts
@@ -0,0 +1,31 @@
+declare global  {
+  interface Technology {
+    name: string;
+    value: 1 | 2 | 3 | 4 | 5;
+  }
+
+  interface Developer {
+    name: string;
+    title: string;
+    phone: string;
+    email: string;
+    technologies?: Technology[];
+    education?: {
+      title: string;
+      year: number;
+    };
+    experience: {
+      title: string;
+      description: string;
+      achievements: string[];
+      technologies?: Technology;
+    }[];
+    projects: {
+      title: string;
+      links?: string;
+      achievements?: string[];
+    }
+  }
+}
+
+export {}
